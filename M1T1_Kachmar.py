18cdb6d9bfcf227ef218cdcc766c1b96351dfe35
kachmark4423 <70001753+kachmark4423@users.noreply.github.com>
2021-08-18 14:05:23 -0500

    Add files via upload

---
+++ M1T1_Kachmar.py
@@ -0,0 +1,15 @@
+# CSC 221
+# M1T1-Orientaion
+# Khalil Kachmar
+# 08-16-2021
+
+def main():
+	name = "Khalil Kachmar\n"
+	languages = ("\nThis semester, python is the only language I am studying formally,\n"
+	"but I am brushing up on SQL and other things needed in the Azure framework for a potential job.\n")
+	misc = "\nFairly recently I got into building miniatures so at present that is my only hobby aside from working out."
+	
+	print(name, languages, misc)
+
+if __name__ == "__main__":
+	main()
